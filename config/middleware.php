185040a71946a321589e799f2c8c8bf67f8b76dc
samuelgfeller <contact@samuel-gfeller.ch>
2024-09-25 13:36:46 +0200

    Updated middleware doc url

--- config/middleware.php
+++ config/middleware.php
@@ -1,7 +1,7 @@
 <?php
 
 // Slim middlewares are LIFO (last in, first out) so when responding, the order is backwards
-// https://samuel-gfeller.ch/docs/Slim-Middleware#order-of-execution
+// https://samuel-gfeller.ch/docs/Slim-Middlewares#order-of-execution
 return function (Slim\App $app) {
     $app->addBodyParsingMiddleware();
 
